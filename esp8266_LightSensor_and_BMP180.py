520eb1722cc927d072557d2bb383b4bd0ba32b22
Radovan Zigo <radovan.zigo@gmail.com>
2020-05-10 23:21:00 +0200

    DeepSleep + WarmUP
    Added DeepSleep 
    Added WarmUP of Pressure sensor

--- esp8266_LightSensor_and_BMP180.py
+++ esp8266_LightSensor_and_BMP180.py
@@ -1,10 +1,10 @@
 ########################
 #                      #
-# 2020-05-06 by Zigi   #
+# 2020-05-10 by Zigi   #
 ########################
 
 sleep_wifi_init=5
-sleep_main_cycle=10
+sleep_main_cycle=30
 
 import network
 import time
@@ -13,7 +13,7 @@ sta_if = network.WLAN(network.STA_IF)
 if not sta_if.isconnected():
     print('connecting to network...')
     sta_if.active(True)
-    sta_if.connect("Wifi", "Passwd")
+    sta_if.connect("Wifi_user", "Wifi_passwd")
     time.sleep(sleep_wifi_init) # waiting for wifi
     while not sta_if.isconnected():
         pass
@@ -34,6 +34,12 @@ from bmp180 import BMP180
 from machine import I2C, Pin
 bus =  I2C(scl=Pin(5), sda=Pin(4), freq=100000)
 
+# configure RTC.ALARM0 to be able to wake the device
+rtc = machine.RTC()
+rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+
+# set RTC.ALARM0 to fire after 10 seconds (waking the device)
+rtc.alarm(rtc.ALARM0, 30000)
 
 
 try:
@@ -51,7 +57,7 @@ except:
 import urequests
 import ubinascii
 
-user_and_pass = str(ubinascii.b2a_base64("%s:%s" % ("InfluxDB_User", "InfluxDB_Passwd"))[:-1], 'utf-8')
+user_and_pass = str(ubinascii.b2a_base64("%s:%s" % ("InfluxDB_user", "InfluxDB_passwd"))[:-1], 'utf-8')
 headers = {'Authorization': 'Basic %s' % user_and_pass}
 #print (headers)
 
@@ -67,22 +73,34 @@ while True:
       LightSensorValue = adc.read()
       
       esp8266_light = 100 - ((LightSensorValue - MIN_VALUE) * 100 / (MAX_VALUE - MIN_VALUE));
-#      esp8266_light = 1024 - LightSensorValue
     
-      temp = bmp180.temperature
+      temp = bmp180.temperature - 8.9   # 8.9 is defauelt error of my sensor :)
+      print("reading pressure")
+      # WarmUP sensor to have better measurement quality
       presure = bmp180.pressure / 100
+      time.sleep(2)
+      presure = bmp180.pressure / 100
+      time.sleep(2)
+      presure = bmp180.pressure / 100
+      time.sleep(2)
+      presure = bmp180.pressure / 100
+      time.sleep(2)
+      presure = bmp180.pressure / 100
+      time.sleep(2)
+      presure = bmp180.pressure / 100
+      print("reading pressure - done")
       altitude = bmp180.altitude
 
     except:
 
       print("Sensor timeout, retrying..")
-
-      time.sleep(sleep_main_cycle)
+      print("Giong to sleep")
+      machine.deepsleep()
 
       continue
 
 
-    url_string = 'https://aaa.com/write?db=DBname'
+    url_string = 'https://domain.com/write?db=DB_name'
     data_string = 'metric=esp8266_light,host=sensor2 value=%s' % (esp8266_light)
     data_string = '%s\nmetric=esp8266_temp,host=sensor2 value=%s' % (data_string, temp)
     data_string = '%s\nmetric=esp8266_presure,host=sensor2 value=%s' % (data_string, presure)
@@ -100,4 +118,5 @@ while True:
       print("Unable to submit data to server")
 
     led.value(1)  # LED OFF
-    time.sleep(sleep_main_cycle)
+    print("Giong to sleep")
+    machine.deepsleep()
